someone5678/device_xiaomi_sweet-commit-archive

sweet: touch: add IHighTouchPollingRate
Change-Id: I75e4bc4386eb5ade0fce2c37b2579875670dc9e3
Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
Signed-off-by: someone5678 <nemui3353@gmail.com>

touch/HighTouchPollingRate.cpp
@@ -1,0 +1,38 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define LOG_TAG "vendor.lineage.touch@1.0-service.xiaomi_sm6150"
+
+#include "HighTouchPollingRate.h"
+
+#include <fstream>
+
+namespace vendor {
+namespace lineage {
+namespace touch {
+namespace V1_0 {
+namespace implementation {
+
+const std::string kHighTouchPollingPath = "/sys/devices/virtual/touch/touch_dev/bump_sample_rate";
+
+Return<bool> HighTouchPollingRate::isEnabled() {
+    std::ifstream file(kHighTouchPollingPath);
+    int enabled;
+    file >> enabled;
+    return enabled == 1;
+}
+
+Return<bool> HighTouchPollingRate::setEnabled(bool enabled) {
+    std::ofstream file(kHighTouchPollingPath);
+    file << (enabled ? "1" : "0");
+    return !file.fail();
+}
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace touch
+}  // namespace lineage
+}  // namespace vendor

touch/service.cpp
@@ -1,0 +1,33 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define LOG_TAG "vendor.lineage.touch@1.0-service.xiaomi_sm6150"
+
+#include <android-base/logging.h>
+#include <hidl/HidlTransportSupport.h>
+
+#include "HighTouchPollingRate.h"
+
+using ::vendor::lineage::touch::V1_0::IHighTouchPollingRate;
+using ::vendor::lineage::touch::V1_0::implementation::HighTouchPollingRate;
+
+int main() {
+    android::sp<IHighTouchPollingRate> highTouchPollingRate = new HighTouchPollingRate();
+
+    android::hardware::configureRpcThreadpool(1, true);
+
+    if (highTouchPollingRate->registerAsService() != android::OK) {
+        LOG(ERROR) << "Cannot register touch high polling rate HAL service.";
+        return 1;
+    }
+
+    LOG(INFO) << "Touch HAL service ready.";
+
+    android::hardware::joinRpcThreadpool();
+
+    LOG(ERROR) << "Touch HAL service failed to join thread pool.";
+    return 1;
+}

Moonlight4004/device_xiaomi_sweet-1

touch/HighTouchPollingRate.cpp
@@ -1,0 +1,38 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define LOG_TAG "vendor.lineage.touch@1.0-service.sweet"
+
+#include "HighTouchPollingRate.h"
+
+#include <fstream>
+
+namespace vendor {
+namespace lineage {
+namespace touch {
+namespace V1_0 {
+namespace implementation {
+
+const std::string kHighTouchPollingPath = "/sys/devices/virtual/touch/touch_dev/bump_sample_rate";
+
+Return<bool> HighTouchPollingRate::isEnabled() {
+    std::ifstream file(kHighTouchPollingPath);
+    int enabled;
+    file >> enabled;
+    return enabled == 1;
+}
+
+Return<bool> HighTouchPollingRate::setEnabled(bool enabled) {
+    std::ofstream file(kHighTouchPollingPath);
+    file << (enabled ? "1" : "0");
+    return !file.fail();
+}
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace touch
+}  // namespace lineage
+}  // namespace vendor

touch/service.cpp
@@ -1,0 +1,33 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define LOG_TAG "vendor.lineage.touch@1.0-service.sweet"
+
+#include <android-base/logging.h>
+#include <hidl/HidlTransportSupport.h>
+
+#include "HighTouchPollingRate.h"
+
+using ::vendor::lineage::touch::V1_0::IHighTouchPollingRate;
+using ::vendor::lineage::touch::V1_0::implementation::HighTouchPollingRate;
+
+int main() {
+    android::sp<IHighTouchPollingRate> highTouchPollingRate = new HighTouchPollingRate();
+
+    android::hardware::configureRpcThreadpool(1, true);
+
+    if (highTouchPollingRate->registerAsService() != android::OK) {
+        LOG(ERROR) << "Cannot register touch high polling rate HAL service.";
+        return 1;
+    }
+
+    LOG(INFO) << "Touch HAL service ready.";
+
+    android::hardware::joinRpcThreadpool();
+
+    LOG(ERROR) << "Touch HAL service failed to join thread pool.";
+    return 1;
+}